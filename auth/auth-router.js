25b2930169777215d508a29c771ed165b488e868
teddyhn <tngo97@gmail.com>
2019-10-16 09:57:12 -0700

    initial

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jsonwebtoken = require("jsonwebtoken");
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +26,10 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +39,20 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    subject: user.id
+  };
+
+  const secret = "super duper secret";
+
+  const options = {
+    expiresIn: "1h"
+  };
+
+  return jsonwebtoken.sign(payload, secret, options);
+}
 
 module.exports = router;
